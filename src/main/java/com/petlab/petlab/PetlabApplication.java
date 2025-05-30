4a443909f951fbbc1d3e7e480bd545bda2323f5a
Kerly Sarrias <kerlysarrias011@gmail.com>
2025-05-30 15:57:13 -0500

    first commit

---
+++ src/main/java/com/petlab/petlab/PetlabApplication.java
@@ -0,0 +1,13 @@
+package com.petlab.petlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petlab/petlab/PetlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petlab.petlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
