e3da7908946f1f8de593a5ea6149875b94b7a940
KerlyVanesaSarrias <157437316+KerlyVanesaSarrias@users.noreply.github.com>
2025-06-09 14:19:02 -0500

    Update WebConfig.java

--- src/main/java/com/petlab/petlab/WebConfig.java
+++ src/main/java/com/petlab/petlab/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://main.d2e3h3wxomf5cy.amplifyapp.com/")
+             /*    .allowedOrigins("https://main.d2e3h3wxomf5cy.amplifyapp.com/") */
+                .allowedOrigins("http://127.0.0.1:5501/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .exposedHeaders("Authorization")
